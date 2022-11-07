127d6e6a00375172f50c24fc00d4a76bb3b255ef
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2022-11-07 20:57:33 +0000

    add reaction intents

--- bot/DiscordClient.py
+++ bot/DiscordClient.py
@@ -5,7 +5,7 @@ import commands.role_menu as rm
 
 class DicordClient(discord.Client):
   def __init__(self, role_menu_channel_id, redis_url):
-    intents = discord.Intents(messages=True, guilds=True, members=True)
+    intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)
     super().__init__(intents= intents)
     self.db = dbManager(redis_url)
     self.rm_channel_id = role_menu_channel_id
